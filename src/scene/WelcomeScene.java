15c5a415caf414c83a6d71a436cbd7b455954c6a
Melvin Macaranas <greatwinblowaway@gmail.com>
2018-11-30 14:14:35 +0700

    add some constructor for welcomescene.java

--- src/scene/WelcomeScene.java
+++ src/scene/WelcomeScene.java
@@ -1,8 +1,22 @@
 package scene;
 
+import javafx.geometry.Pos;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.VBox;
 
 public class WelcomeScene extends VBox {
 	
+	public WelcomeScene() {
+		
+		this.setAlignment(Pos.CENTER);
+		//this.setBackground(new Background(arg0));
+		Canvas canvas = new Canvas();
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		
+		VBox buttonBox = new VBox();
+		
+	}
 	
 }
