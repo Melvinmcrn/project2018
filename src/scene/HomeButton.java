e3a66e7ca1a6f682e1e4fa9764d4e56ad06ebe4e
Melvin Macaranas <greatwinblowaway@gmail.com>
2018-12-03 14:25:39 +0700

    edit welcomescene and HomeButton

---
+++ src/scene/HomeButton.java
@@ -0,0 +1,19 @@
+package scene;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.text.Font;
+
+public class HomeButton extends Canvas{
+
+	private String goToScene;
+	private String text;
+	private static double width = 100;
+	private static double height = 100;
+	private Font TEXT_FONT = new Font("Tahoma", 25);
+		
+	public HomeButton(String text) {
+		super(height, width);
+		this.
+	}
+	
+}
