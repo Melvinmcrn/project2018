dc473811d82ee3ed0a41b9e2b48230969eeb7620
melvinmcrn <greatwinblowaway@gmail.com>
2018-12-07 01:11:58 +0700

    implement table.java

--- src/area/Table.java
+++ src/area/Table.java
@@ -1,8 +1,12 @@
 package area;
 
 import customer.*;
+import javafx.event.EventHandler;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import logic.GameLogic;
 
@@ -14,13 +18,11 @@ public class Table extends Pane {
 	private Customer customer = null;
 	private String customerName = null;
 	private int money;
+	private boolean isClicked;
+	private String action = null;
 
-	private static String tableNormal;
-	private static String tableDoraemon;
-	private static String tableNobita;
-	private static String tableGiant;
-	private static String tableShizuka;
-	private static String tableSuneo;
+	private static final String tableNormal = ClassLoader.getSystemResource("images/TableNormal.png").toString();
+	private static final String tableNormalBW = ClassLoader.getSystemResource("images/TableNormalBw.png").toString();
 	private ImageView image;
 
 	public Table(int x, int y) {
@@ -29,36 +31,95 @@ public class Table extends Pane {
 		this.isAvailable = true;
 		this.money = 0;
 		this.image = new ImageView(tableNormal);
+		this.image.setX(x * 80);
+		this.image.setY(y * 80);
+		this.isClicked = false;
+		this.setEvent();
+
+		this.getChildren().add(image);
 	}
 
 	private void leave() {
 		this.customer = null;
 		this.customerName = null;
+		this.action = null;
 		this.isAvailable = true;
 		this.money = 0;
-		this.setImage();
+		this.setImage("");
+	}
+	
+	private void eat() {
+		this.action = "Eat";
+		
 	}
 
-	private void setImage() {
-		if(this.isAvailable) {
-			this.image.setImage(new Image(tableNormal));
-		} else if(this.customerName.equals("Doraemon")) {
-			this.image.setImage(new Image(tableDoraemon));
-		} else if(this.customerName.equals("Nobita")) {
-			this.image.setImage(new Image(tableNobita));
-		} else if(this.customerName.equals("Giant")) {
-			this.image.setImage(new Image(tableGiant));
-		} else if(this.customerName.equals("Shizuka")) {
-			this.image.setImage(new Image(tableShizuka));
-		} else if(this.customerName.equals("Suneo")) {
-			this.image.setImage(new Image(tableSuneo));
+	private void setImage(String action) {
+		if (this.isAvailable) {
+			if (action.equals("Bw")) {
+				this.image.setImage(new Image(tableNormalBW));
+			} else {
+				this.image.setImage(new Image(tableNormal));
+			}
+		} else {
+			this.image.setImage(new Image(this.getImagePath(action)));
+			if (action.equals("Sit") || action.equals("SitBw") || action.equals("Eat") || action.equals("EatBw")
+					|| action.equals("Bill") || action.equals("BillBw")) {
+				this.image.setX((this.x - 1) * 80);
+				this.image.setY((this.y - 1) * 80);
+			} else {
+				this.image.setX((this.x) * 80);
+				this.image.setY((this.y) * 80);
+			}
 		}
+	}
+
+	private String getImagePath(String action) {
+		if (!this.isAvailable) {
+			return ClassLoader
+					.getSystemResource("images/" + this.customerName + "/" + this.customerName + action + ".png")
+					.toString();
+		} else {
+			return Table.tableNormal;
+		}
+	}
+
+	private void setEvent() {
+		this.image.setOnMouseEntered(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+
+			}
+
+		});
+
+		this.image.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				System.out.println("Click table X:"+x+" Y:"+y);
+				if(isClicked) {
+					setImage("");
+					isClicked = false;
+				} else {
+					setImage("Bw");
+					isClicked = true;
+				}
+
+			}
+		});
+
+		this.image.setOnMouseDragOver(new EventHandler<MouseDragEvent>() {
+			@Override
+			public void handle(MouseDragEvent event) {
+
+			}
+		});
 	}
 
 	public void sit(Customer customer) {
 		if (this.isAvailable) {
 			this.customer = customer;
 			this.customerName = customer.getName();
+			this.action = "Sit";
 			this.isAvailable = false;
 		}
 	}
--- src/gameScene/MainGame.java
+++ src/gameScene/MainGame.java
@@ -1,17 +1,13 @@
 package gameScene;
 
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
+import area.Table;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.Pane;
-import sharedObject.IRenderable;
-import sharedObject.RenderableHolder;
 
 public class MainGame extends Pane {
 	
-	private GraphicsContext gc;
 	private Image bg = new Image(ClassLoader.getSystemResource("images/GameBackground.png").toString());
 	
 	public MainGame() {
@@ -19,13 +15,10 @@ public class MainGame extends Pane {
 		this.setPrefWidth(800);
 		this.setBackground(new Background(new BackgroundImage(bg, null, null, null, null)));
 		this.setVisible(true);
-		//this.paintComponent();
+		
+		Table table1 = new Table(2,2);
+		
+		this.getChildren().add(table1);
 	}
 	
-	public void paintComponent() {
-		//gc = this.getGraphicsContext2D();
-		for(IRenderable entity : RenderableHolder.getInstance().getEntities()) {
-			entity.draw(gc);
-		}
-	}
 }
